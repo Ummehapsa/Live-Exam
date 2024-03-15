7dc074ea7e6ee3240bd0cf0c0ae49d8348222672
Umme Hapsa <128204321+Ummehapsa@users.noreply.github.com>
2024-03-16 00:46:26 +0600

    Update LiveExam.dart

---
+++ LiveExam.dart
@@ -0,0 +1,32 @@
+void main() {
+  Map<String, Map<String, double>> Apple = {
+    'Apple' : {
+      'Red' : 2.5}};
+
+  Map<String, Map<String, double>> Orange = {
+    'Orange' : {
+      'Orange' : 1.0}};
+
+  Map<String, Map<String, double>> Banana = {
+    'Banana' : {
+      'Yellow' : 2.0}};
+
+  print(Apple);
+  print(Orange);
+  print(Banana);
+
+}
+
+
+
+
+void fruitList (Apple, Orange, Banana) {
+  print(Apple);
+  print(Orange);
+  print(Banana);
+}
+
+
+void percentage () {
+  
+}
