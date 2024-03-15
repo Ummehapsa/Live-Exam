5dedeb0b7d46e145e888dfd28a8891658bc593ed
Umme Hapsa <128204321+Ummehapsa@users.noreply.github.com>
2024-03-16 03:40:52 +0600

    Update LiveExam.dart

--- LiveExam.dart
+++ LiveExam.dart
@@ -7,20 +7,22 @@ void main() {
 
   displayFruitDetails(fruits);
   applyPriceDiscount(fruits, 10);
+
+  print('After applying discount:');
   displayFruitDetails(fruits);
 }
 
 void displayFruitDetails(List<Map<String, dynamic>> fruits) {
   print('Fruit Details:');
-  for (var fruit in fruits) {
-    print('Name: ${fruit['name']}, Color: ${fruit['color']}, Price: \$${fruit['price']}');
+  for (var item in fruits) {
+    print('Name: ${item['name']}, Color: ${item['color']}, Price: \$${item['price']}');
   }
 }
 
 void applyPriceDiscount(List<Map<String, dynamic>> fruits, double discountPercentage) {
-  for (var fruit in fruits) {
-    double originalPrice = fruit['price'];
-    double discountedPrice = originalPrice * ( discountPercentage / 100);
-    fruit['price'] = discountedPrice;
+  for (var item in fruits) {
+    double originalPrice = item['price'];
+    double discountedPrice = originalPrice * discountPercentage / 100;
+    item['price'] = discountedPrice.toStringAsFixed(2);
   }
 }
